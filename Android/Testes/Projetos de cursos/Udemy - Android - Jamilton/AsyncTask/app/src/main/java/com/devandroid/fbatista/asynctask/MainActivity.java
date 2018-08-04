2163ec935729364c6d9cdef26f3b64f427e54c71
Fernando Batista <fernandohsbatista@gmail.com>
2018-08-04 13:47:25 -0300

    novo projeto asynctask

---
+++ Android/Testes/Projetos de cursos/Udemy - Android - Jamilton/AsyncTask/app/src/main/java/com/devandroid/fbatista/asynctask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.devandroid.fbatista.asynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
